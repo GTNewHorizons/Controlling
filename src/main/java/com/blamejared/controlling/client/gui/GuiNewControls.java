1a85dccf9dcaa15dbe46b47442a48040333d9258
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2023-02-06 00:14:22 +0100

    comment out http request whose result isn't used

--- src/main/java/com/blamejared/controlling/client/gui/GuiNewControls.java
+++ src/main/java/com/blamejared/controlling/client/gui/GuiNewControls.java
@@ -12,8 +12,6 @@ import net.minecraft.client.settings.GameSettings;
 import net.minecraft.client.settings.KeyBinding;
 import net.minecraft.util.StatCollector;
 
-import com.blamejared.controlling.Controlling;
-
 import cpw.mods.fml.client.config.GuiCheckBox;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
@@ -135,9 +133,9 @@ public class GuiNewControls extends GuiControls {
                 false);
         this.buttonList.add(this.buttonCat);
 
-        this.name = Controlling.PATRON_LIST.stream()
-                .skip(Controlling.PATRON_LIST.isEmpty() ? 0 : new Random().nextInt(Controlling.PATRON_LIST.size()))
-                .findFirst().orElse("");
+        // this.name = Controlling.PATRON_LIST.stream()
+        // .skip(Controlling.PATRON_LIST.isEmpty() ? 0 : new Random().nextInt(Controlling.PATRON_LIST.size()))
+        // .findFirst().orElse("");
 
         this.sortOrderButton = new GuiButton(
                 1008,
