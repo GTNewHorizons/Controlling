76319764eb13fbc901087c724e860a485552c7ad
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2023-02-06 18:45:21 +0100

    just delete the code

--- src/main/java/com/blamejared/controlling/Controlling.java
+++ src/main/java/com/blamejared/controlling/Controlling.java
@@ -10,26 +10,6 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 @Mod(modid = "controlling", name = "Controlling", version = "GRADLETOKEN_VERSION", acceptableRemoteVersions = "*")
 public class Controlling {
 
-    // public static Set<String> PATRON_LIST = new HashSet<>();
-
-    // public Controlling() {
-    // new Thread(() -> {
-    // try {
-    // URL url = new URL("https://blamejared.com/patrons.txt");
-    // URLConnection urlConnection = url.openConnection();
-    // urlConnection.setConnectTimeout(15000);
-    // urlConnection.setReadTimeout(15000);
-    // urlConnection.setRequestProperty("User-Agent", "Controlling|1.7.10");
-    // try (BufferedReader reader = new BufferedReader(
-    // new InputStreamReader(urlConnection.getInputStream()))) {
-    // PATRON_LIST = reader.lines().filter(s -> !s.isEmpty()).collect(Collectors.toSet());
-    // }
-    // } catch (IOException e) {
-    // e.printStackTrace();
-    // }
-    // }).start();
-    // }
-
     @Mod.EventHandler
     private void init(final FMLInitializationEvent event) {
         if (event.getSide().isClient()) {
