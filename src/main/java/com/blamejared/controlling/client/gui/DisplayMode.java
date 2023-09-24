46d1cda341a635ef236cd2a3e410eb880098f2f5
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2023-09-24 22:08:18 +0200

    fix : alphabetical sorting of keys not working

--- src/main/java/com/blamejared/controlling/client/gui/DisplayMode.java
+++ src/main/java/com/blamejared/controlling/client/gui/DisplayMode.java
@@ -7,27 +7,30 @@ import net.minecraft.client.settings.KeyBinding;
 
 public enum DisplayMode {
 
-    ALL(keyEntry -> true),
-    UNBOUND(keyEntry -> keyEntry.getKeybinding().getKeyCode() == 0),
-    CONFLICTING(keyEntry -> {
-        for (KeyBinding key : Minecraft.getMinecraft().gameSettings.keyBindings) {
-            if (!key.getKeyDescription().equals(keyEntry.getKeybinding().getKeyDescription())
-                    && key.getKeyCode() != 0) {
-                if (key.getKeyCode() == keyEntry.getKeybinding().getKeyCode()) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    });
-
-    private final Predicate<GuiNewKeyBindingList.KeyEntry> predicate;
-
-    DisplayMode(Predicate<GuiNewKeyBindingList.KeyEntry> predicate) {
-        this.predicate = predicate;
-    }
+    ALL,
+    CONFLICTING,
+    UNBOUND;
 
     public Predicate<GuiNewKeyBindingList.KeyEntry> getPredicate() {
-        return predicate;
+        switch (this) {
+            case ALL:
+                return keyEntry -> true;
+            case CONFLICTING:
+                return keyEntry -> {
+                    for (KeyBinding key : Minecraft.getMinecraft().gameSettings.keyBindings) {
+                        if (!key.getKeyDescription().equals(keyEntry.getKeybinding().getKeyDescription())
+                                && key.getKeyCode() != 0) {
+                            if (key.getKeyCode() == keyEntry.getKeybinding().getKeyCode()) {
+                                return true;
+                            }
+                        }
+                    }
+                    return false;
+                };
+            case UNBOUND:
+                return keyEntry -> keyEntry.getKeybinding().getKeyCode() == 0;
+        }
+        throw new IllegalStateException();
     }
+
 }
